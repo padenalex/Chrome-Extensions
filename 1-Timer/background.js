33b370d4c321f3998d560e190b252d27543e79ba
IconianGate <alexpaden1@gmail.com>
2021-10-27 13:23:48 -0600

    🏁🍅 - Finished basic pomo
    Includes:
    > Manage Tasks +/-
    > Display Timer countdown
    > Styling
    > Alarm
    > Stop when time is up
    
    summary: kind of boring but a nice look at extension background scripts and so forth.

--- 1-Timer/background.js
+++ 1-Timer/background.js
@@ -4,21 +4,31 @@ chrome.alarms.create("pomodoroTimer", {
 
 chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "pomodoroTimer"){
-        chrome.storage.local.get(["timer", "isRunning"], (res) => {
+        chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
             if (res.isRunning) {
                 let timer = res.timer + 1
-                console.log(timer)
+                let isRunning = true
+                if (timer === 60 * res.timeOption) {
+                    this.registration.showNotification(" Pomo timer is up", {
+                        body: `${res.timeOption} minutes has passed`,
+                        icon: "icon.png"
+                    })
+                    timer = 0
+                    isRunning = false
+                }
                 chrome.storage.local.set({
                     timer,
+                    isRunning,
                 })
             }
         })
     }
 })
 
-chrome.storage.local.get(["timer", "isRunning"], (res) => {
+chrome.storage.local.get(["timer", "isRunning", "timeOption"], (res) => {
     chrome.storage.local.set({
         timer: "timer" in res ? res.timer : 0,
         isRunning: "isRunning" in res ? res.isRunning : false,
+        timeOption: "timeOption" in res ? res.timeOption : 25,
     })
 })
